d5f9f1afbd6cda938f02d6ec0a0b400822c5b5f1
Ian Morland <ian@morland.me>
2023-03-24 15:28:28 +0000

    chore: use frontend extenders

--- js/src/forum/components/ProfilePage.js
+++ js/src/forum/components/ProfilePage.js
@@ -4,6 +4,8 @@ import Button from 'flarum/common/components/Button';
 import username from 'flarum/common/helpers/username';
 import UserPage from 'flarum/forum/components/UserPage';
 import Stream from 'flarum/common/utils/Stream';
+import Placeholder from 'flarum/common/components/Placeholder';
+import LoadingIndicator from 'flarum/common/components/LoadingIndicator';
 
 export default class ProfilePage extends UserPage {
   oninit(vdom) {
@@ -14,9 +16,27 @@ export default class ProfilePage extends UserPage {
     this.ignoredUsers = app.session.user.ignoredUsers();
 
     this.loadUser(app.session.user.username());
+
+    this.loading = false;
   }
 
   content() {
+    if (this.loading) {
+      return (
+        <div className="DiscussionList">
+          <LoadingIndicator />
+        </div>
+      );
+    }
+
+    if (this.ignoredUsers.length === 0) {
+      return (
+        <div className="DiscussionList">
+          <Placeholder text={app.translator.trans('fof-ignore-users.forum.profile_page.no_ignored')} />
+        </div>
+      );
+    }
+
     return (
       <table className="NotificationGrid">
         {this.ignoredUsers.map((user, i) => {
